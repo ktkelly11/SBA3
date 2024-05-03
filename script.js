ebf0f96d75fd79eee0e769a5ec09dfc618c77c2e
Katherine Kelly <ktkelly11@gmail.com>
2024-05-02 20:04:11 -0400

    starting to cache elements

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 // Examining the document
 // console.dir(document);
+
+const header = document.getElementsByClassName("header");
+const intro = document.querySelector(".introduction");
+const donations = document.getElementById("donation-forms");
+console.log(donations);
